b25caca547f8f8608f64e9b1adf59566a59e404a
kaushaldeokar <kaushal.deokar@gmail.com>
2023-01-25 01:00:49 +0530

    Revert "Revert "second""
    This reverts commit 74950ec48bb392d3d600a966a42837d7e21f464f.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import "./App.css";
+import NavBar from "./component/NavBar";
 
-import './App.css';
-import NavBar from './component/NavBar';
-
+import React, { useState } from "react";
 function App() {
+  const [Task, setTask] = useState("");
+
+  
+
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <div className="input">
+        <form>
+          <div className="form-group">
+            <label
+              htmlFor="exampleInputEmail1"
+              style={{ backgroundColor: "DodgerBlue" }}
+            >
+              Add Your Task
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-success"
+            //onClick={}//
+          >
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
