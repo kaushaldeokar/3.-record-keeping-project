b52c9c9366ae9c4177e97e50dc1784561cd32c63
kaushaldeokar <kaushal.deokar@gmail.com>
2023-01-29 16:34:30 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,37 +5,10 @@ import React, { useState } from "react";
 function App() {
   const [Task, setTask] = useState("");
 
-  
-
   return (
     <div className="App">
       <NavBar />
-      <div className="input">
-        <form>
-          <div className="form-group">
-            <label
-              htmlFor="exampleInputEmail1"
-              style={{ backgroundColor: "DodgerBlue" }}
-            >
-              Add Your Task
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="btn btn-success"
-            //onClick={}//
-          >
-            Add
-          </button>
-        </form>
-      </div>
+      
     </div>
   );
 }
